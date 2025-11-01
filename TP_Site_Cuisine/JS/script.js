5f652848b95d931099a13c535b0573b4fe7e3b1e
DorineCo <dorinecoquette@hotmail.com>
2025-11-01 12:12:12 +0100

    mise à jour - suppression TP cuisine

--- TP_Site_Cuisine/JS/script.js
+++
@@ -1,7 +0,0 @@
-// Menu burger toggle
-const burger = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
-
-burger.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-});
