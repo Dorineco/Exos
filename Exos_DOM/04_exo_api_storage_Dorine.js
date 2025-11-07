826efa90a7944eec04a5cb431d8481a89ab62ec7
DorineCo <dorinecoquette@hotmail.com>
2025-11-07 17:04:15 +0100

    mise à jour

--- Exos_DOM/04_exo_api_storage_Dorine.js
+++ Exos_DOM/04_exo_api_storage_Dorine.js
@@ -4,14 +4,14 @@ const filmContainer = document.getElementById('film-container');
 const storageContent = document.getElementById('storage-content');
 
 btn.addEventListener('click', () => {
-    const query = document.getElementById('FilmID').value;
+    const query = document.getElementById('filmID').value;
 
     //Appel API
     fetch(`https://imdb.iamidiotareyoutoo.com/search?q=${query}`)
         .then(response => response.json())
         .then(data => {
             localStorage.setItem(query, JSON.stringify(data.description)); //Stockage dans le localStorage
-            const films = JSON.parse(localStorage.getItem(query)); //fetch Local storage
+            const films = JSON.parse(localStorage.getItem(query)); //récupère localStorage
 
 
             //Affichage du film
@@ -25,25 +25,25 @@ btn.addEventListener('click', () => {
             });
         })
 })
+//Création du lien vers les détails du film
 
-import { of } from 'rxjs';
-of('Données').subscribe(data => console.log(data));
-
-
+filmDiv.addEventListener('click', () => {showFilmDetails(film['#IMDB_ID'])});
 
 
+    function afficherFilmDetails(imdbId) {
+    fetch(`https://imdb.iamidiotareyoutoo.com/search?tt=${imdbId}`)
+        .then(response => response.json())
+        .then(data => {const film = data.short;  
+    afficherFilmDetails(film);
+    });
 
 
+function FilmDetailsshort(film) {
+    const detailsContainer = document.getElementById('details');
+    detailsContainer.innerHTML = `
+        <h3>${film.name}</h3>
+        <img src="${film.image}" alt="${film.name}" width="200">
+        <p><strong>Description :</strong> ${film.description}</p>
+        <p><a href="${film.url}" target="_blank">Voir sur IMDb</a></p>`;
 
-
-
-
-
-
-
-
-
-
-
-
-
+}
